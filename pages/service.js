67e3fb410e7ebaec2f0dfb0b660a3b8635c6cc8f
IsoBob1 <isobell.allanson@gmail.com>
2024-10-30 19:26:40 +1100

    Update service.js

--- pages/service.js
+++ pages/service.js
@@ -3,39 +3,39 @@ import Layout from '../components/Layout';
 import { ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LabelList } from 'recharts';
 
 const data = [
-  { time: '14:00', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '14:15', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '14:30', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '14:45', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '15:00', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '15:15', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '15:30', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '15:45', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '16:00', arriving: 0, inVenue: 0, staffCount: 2 },
-  { time: '16:15', arriving: 0, inVenue: 0, staffCount: 2 },
-  { time: '16:30', arriving: 0, inVenue: 0, staffCount: 2 },
-  { time: '16:45', arriving: 0, inVenue: 0, staffCount: 2 },
-  { time: '17:00', arriving: 4, inVenue: 4, staffCount: 2 },
-  { time: '17:15', arriving: 0, inVenue: 4, staffCount: 2 },
-  { time: '17:30', arriving: 0, inVenue: 4, staffCount: 3 },
-  { time: '17:45', arriving: 2, inVenue: 6, staffCount: 3 },
-  { time: '18:00', arriving: 10, inVenue: 16, staffCount: 4 },
-  { time: '18:15', arriving: 0, inVenue: 16, staffCount: 5 },
-  { time: '18:30', arriving: 9, inVenue: 25, staffCount: 5 },
-  { time: '18:45', arriving: 0, inVenue: 25, staffCount: 5 },
-  { time: '19:00', arriving: 2, inVenue: 23, staffCount: 5 },
-  { time: '19:15', arriving: 6, inVenue: 29, staffCount: 5 },
-  { time: '19:30', arriving: 2, inVenue: 31, staffCount: 5 },
-  { time: '19:45', arriving: 0, inVenue: 25, staffCount: 5 },
-  { time: '20:00', arriving: 0, inVenue: 21, staffCount: 5 },
-  { time: '20:15', arriving: 0, inVenue: 21, staffCount: 5 },
-  { time: '20:30', arriving: 0, inVenue: 10, staffCount: 4 },
-  { time: '20:45', arriving: 0, inVenue: 10, staffCount: 4 },
-  { time: '21:00', arriving: 0, inVenue: 10, staffCount: 3 },
-  { time: '21:15', arriving: 0, inVenue: 2, staffCount: 2 },
-  { time: '21:30', arriving: 0, inVenue: 0, staffCount: 2 },
-  { time: '21:45', arriving: 0, inVenue: 0, staffCount: 1 },
-  { time: '22:00', arriving: 0, inVenue: 0, staffCount: 1 }
+  { time: '14:00', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 }, // Nicola starts
+  { time: '14:15', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 },
+  { time: '14:30', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 },
+  { time: '14:45', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 },
+  { time: '15:00', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 },
+  { time: '15:15', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 },
+  { time: '15:30', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 },
+  { time: '15:45', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 },
+  { time: '16:00', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 1 }, // Kieran starts
+  { time: '16:15', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 1 },
+  { time: '16:30', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 1 },
+  { time: '16:45', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 1 },
+  { time: '17:00', arriving: 4, inVenue: 4, floorStaff: 1, barStaff: 1 },
+  { time: '17:15', arriving: 0, inVenue: 4, floorStaff: 1, barStaff: 1 },
+  { time: '17:30', arriving: 0, inVenue: 4, floorStaff: 2, barStaff: 1 }, // Aurelie starts
+  { time: '17:45', arriving: 2, inVenue: 6, floorStaff: 2, barStaff: 1 },
+  { time: '18:00', arriving: 10, inVenue: 16, floorStaff: 3, barStaff: 1 }, // Will starts
+  { time: '18:15', arriving: 0, inVenue: 16, floorStaff: 4, barStaff: 1 }, // Elias starts
+  { time: '18:30', arriving: 9, inVenue: 25, floorStaff: 4, barStaff: 1 },
+  { time: '18:45', arriving: 0, inVenue: 25, floorStaff: 4, barStaff: 1 },
+  { time: '19:00', arriving: 2, inVenue: 23, floorStaff: 4, barStaff: 1 },
+  { time: '19:15', arriving: 6, inVenue: 29, floorStaff: 4, barStaff: 1 },
+  { time: '19:30', arriving: 2, inVenue: 31, floorStaff: 4, barStaff: 1 },
+  { time: '19:45', arriving: 0, inVenue: 25, floorStaff: 4, barStaff: 1 },
+  { time: '20:00', arriving: 0, inVenue: 21, floorStaff: 4, barStaff: 1 },
+  { time: '20:15', arriving: 0, inVenue: 21, floorStaff: 4, barStaff: 1 },
+  { time: '20:30', arriving: 0, inVenue: 10, floorStaff: 3, barStaff: 1 },
+  { time: '20:45', arriving: 0, inVenue: 10, floorStaff: 3, barStaff: 1 },
+  { time: '21:00', arriving: 0, inVenue: 10, floorStaff: 2, barStaff: 1 },
+  { time: '21:15', arriving: 0, inVenue: 2, floorStaff: 1, barStaff: 1 },
+  { time: '21:30', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 1 },
+  { time: '21:45', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 },
+  { time: '22:00', arriving: 0, inVenue: 0, floorStaff: 1, barStaff: 0 }
 ];
 
 export default function ServiceAnalysis() {
@@ -64,13 +64,22 @@ export default function ServiceAnalysis() {
             <Tooltip />
             <Legend />
             <Bar 
-              yAxisId="left"
-              dataKey="arriving" 
-              name="Covers Arriving" 
+              yAxisId="right"
+              dataKey="floorStaff" 
+              name="Floor Staff" 
               fill="#82ca9d"
-              opacity={0.7}
+              stackId="staff"
             >
-              <LabelList dataKey="arriving" position="inside" />
+              <LabelList dataKey="floorStaff" position="inside" fill="white" />
+            </Bar>
+            <Bar 
+              yAxisId="right"
+              dataKey="barStaff" 
+              name="Bar Staff" 
+              fill="#8884d8"
+              stackId="staff"
+            >
+              <LabelList dataKey="barStaff" position="inside" fill="white" />
             </Bar>
             <Line 
               yAxisId="left"
@@ -85,21 +94,6 @@ export default function ServiceAnalysis() {
                 dataKey="inVenue" 
                 position="top"
                 offset={10}
-              />
-            </Line>
-            <Line 
-              yAxisId="right"
-              type="stepAfter" 
-              dataKey="staffCount" 
-              name="Staff on Floor" 
-              stroke="#8884d8"
-              strokeWidth={2}
-              dot={{ r: 3 }}
-            >
-              <LabelList 
-                dataKey="staffCount" 
-                position="right"
-                offset={5}
               />
             </Line>
           </ComposedChart>
